fac6ecccaced08c447670a68085d358647d2a236
luciano_bestia <info@bestia.dev>
2024-03-19 17:48:08 -0500

    2024-03-19

--- src/bin/gajpg/main.rs
+++ src/bin/gajpg/main.rs
@@ -41,7 +41,7 @@ fn print_help() {
 gajpg --help
 gajpg image1.jpg
   
-    © 2023 bestia.dev  MIT License github.com/bestia-dev/cargo-auto
+    © 2023 bestia.dev  MIT License github.com/automation-tasks-rs/cargo-auto
 "#
     );
 }
